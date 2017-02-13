380f94cdeb8326fb4158ff5447f2c8dc065d7940
Tyler Rose <tylerjosephrose@gmail.com>
2017-02-13 17:24:01 -0500

    Initial Commit

---
+++ BidEuchre/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BidEuchre
+//
+//  Created by Tyler Rose on 2/13/17.
+//  Copyright © 2017 Tyler Rose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
